bbe3483143f34617af961c75d60660ee5c8aa614
Smi <smilja.knezev@gmail.com>
2021-06-14 21:54:42 +0200

    dodaj zadatak

---
+++ TreciPopravni/src/Product.java
@@ -0,0 +1,18 @@
+
+public class Product {
+	
+	/*calcultes product of first n elements
+	 * 
+	 * n needs to be a positive number
+	 * 
+	 * */
+	
+	public int product(int n) {
+		int result = 1;
+		for(int i = 0; i < n; i++) {
+			result = i;
+		}
+		return result;
+	}
+
+}
